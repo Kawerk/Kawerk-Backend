df999fa693b941e3f5a0f4acabe23464a6b10b15
Your Name <you@example.com>
2025-11-30 12:46:24 +0200

    phase1 transaction backend

---
+++ Kawerk/Application/Interfaces/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using Kawerk.Infastructure.DTOs;
+
+namespace Kawerk.Application.Interfaces
+{
+    public interface ITransactionService
+    {
+        public Task<int> CreateTransaction(TransactionCreationDTO transaction);
+        public Task<int> DeleteTransaction(Guid transactionID);
+        public Task<TransactionDTO> GetTransaction(Guid transactionID);
+        public Task<List<TransactionDTO>> GetTransactions();
+
+    }
+}
---
+++ Kawerk/Application/Services/TransactionService.cs
@@ -0,0 +1,117 @@
+﻿using Kawerk.Application.Interfaces;
+using Kawerk.Domain;
+using Kawerk.Infastructure.Context;
+using Kawerk.Infastructure.DTOs;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace Kawerk.Application.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly DbBase _db;
+        public TransactionService(DbBase db)
+        {
+            _db = db;
+        }
+        public async Task<int> CreateTransaction(TransactionCreationDTO transaction)
+        {
+            if (transaction == null)
+                return 0;
+
+            if (transaction.SellerCustomerID != Guid.Empty)//0 == Faulty DTO || 1 == emtpy SellerID and ManufacturerID || 2 == Seller not found || 3 == Manufacturer not found || 4 == Customer not found || 5 == Vehicle not found || 6 == Successful
+            {
+                var isSellerExists = await (from c in _db.Customers
+                                              where c.CustomerID == transaction.SellerCustomerID
+                                              select c).FirstOrDefaultAsync();
+                if (isSellerExists == null)
+                    return 2;
+                var isCustomerExists = await (from c in _db.Customers
+                                                where c.CustomerID == transaction.BuyerID
+                                                select c).FirstOrDefaultAsync();
+                if(isCustomerExists == null)
+                    return 4;
+
+                var isVehicleExists = await (from v in _db.Vehicles
+                                             where v.VehicleID == transaction.VehicleID
+                                             select v).FirstOrDefaultAsync();
+                if (isVehicleExists == null)
+                    return 5;
+
+                Domain.Transaction newTransaction = new Domain.Transaction
+                {
+                    TransactionID = Guid.NewGuid(),
+                    Buyer = isCustomerExists,
+                    SellerCustomer = isSellerExists,
+                    Amount = transaction.Amount,
+                    CreatedDate = DateTime.Now,
+                    Vehicle = isVehicleExists
+                };
+                await _db.Transactions.AddAsync(newTransaction);
+                await _db.SaveChangesAsync();
+                return 6;
+            }
+            else if (transaction.SellerManufacturerID != Guid.Empty)
+            {
+                var isManufacturerExists = await (from m in _db.Manufacturers
+                                            where m.ManufacturerID == transaction.SellerManufacturerID
+                                            select m).FirstOrDefaultAsync();
+                if (isManufacturerExists == null)
+                    return 3;
+
+                var isCustomerExists = await (from c in _db.Customers
+                                              where c.CustomerID == transaction.BuyerID
+                                              select c).FirstOrDefaultAsync();
+                if (isCustomerExists == null)
+                    return 4;
+
+                var isVehicleExists = await (from v in _db.Vehicles
+                                             where v.VehicleID == transaction.VehicleID
+                                             select v).FirstOrDefaultAsync();
+                if (isVehicleExists == null)
+                    return 5;
+
+                Domain.Transaction newTransaction = new Domain.Transaction
+                {
+                    TransactionID = Guid.NewGuid(),
+                    Buyer = isCustomerExists,
+                    SellerManufacturer = isManufacturerExists,
+                    Amount = transaction.Amount,
+                    CreatedDate = DateTime.Now,
+                    Vehicle = isVehicleExists
+                };
+                await _db.Transactions.AddAsync(newTransaction);
+                await _db.SaveChangesAsync();
+                return 6;
+            }
+            else
+                return 1;
+        }
+
+        public async Task<int> DeleteTransaction(Guid transactionID)
+        {
+            if (transactionID == Guid.Empty)
+                return 0;
+            var isTransactionExists = await (from t in _db.Transactions
+                                     where t.TransactionID == transactionID
+                                     select t).FirstOrDefaultAsync();
+
+            if (isTransactionExists == null)
+                return 1;
+
+            _db.Transactions.Remove(isTransactionExists);
+            await _db.SaveChangesAsync();
+            return 2;
+        }
+
+        public Task<TransactionDTO> GetTransaction(Guid transactionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TransactionDTO>> GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
